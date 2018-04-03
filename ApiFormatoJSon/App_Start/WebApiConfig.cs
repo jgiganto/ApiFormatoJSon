5dea3b166a1a84957a3e2ec856c0fbb6853c2fc0
jgiganto <jgiganto1@hotmail.com>
2018-04-03 10:54:17 +0200

    s

--- ApiFormatoJSon/App_Start/WebApiConfig.cs
+++ ApiFormatoJSon/App_Start/WebApiConfig.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web.Http;
 
@@ -10,6 +12,8 @@ namespace ApiFormatoJSon
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.Remove(config.Formatters.XmlFormatter); //eliminar el formato xml
+            ConfigurarFormatoJson(config);//siempre indicarlo antes del mapeo
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -20,5 +24,30 @@ namespace ApiFormatoJSon
                 defaults: new { id = RouteParameter.Optional }
             );
         }
+
+        public static void ConfigurarFormatoJson(HttpConfiguration config)
+        {
+            //capturamos el formato json para darle otra salida
+            //RECUPERAMOS EL FORMATO
+            var formato = config.Formatters.JsonFormatter;
+
+            //PONEMOS TODAS LAS PROPIEDADES A MINUSCULA
+            formato.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
+
+            //INDICAMOS QUE NO QUEREMOS MOSTRAR VALORES NULOS
+            formato.SerializerSettings.NullValueHandling =
+                Newtonsoft.Json.NullValueHandling.Ignore;
+
+            //CAMBIAR EL FORMATO DE FECHA
+            formato.SerializerSettings.DateFormatHandling =
+                Newtonsoft.Json.DateFormatHandling.MicrosoftDateFormat;
+
+            //CAMBIAR LA CULTURA DE LO QUE EXPONEMOS
+            //A ESPAÑA, COMO POR EJEMPLO MONEDAS
+            formato.SerializerSettings.Culture = new CultureInfo("es-ES");
+
+        }
     }
+
 }
---
+++ ApiFormatoJSon/Controllers/EmpleadosController.cs
@@ -0,0 +1,63 @@
+﻿using ApiFormatoJSon.Models;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+//espacio de nombres para tareas Async
+using System.Threading; //Generico(de toda la vida..un poco antiguo)
+//nuevo espacio para TASK(moderno)
+using System.Threading.Tasks;
+
+namespace ApiFormatoJSon.Controllers
+{
+    public class EmpleadosController : ApiController
+    {
+        ModeloEmpleados modelo;
+        public EmpleadosController()
+        {
+            this.modelo = new ModeloEmpleados();
+        }
+
+        //cuando devolvemos datos, debemos utilizar TASK<>
+        public Task<List<EMP>> GetEmpleados()
+        {
+            List<EMP> empleados = modelo.GetEmpleados();
+            //para devolver datos en TASK, se utuliza el metodo .fromResult<tipodato>(objeto)
+            return Task.FromResult<List<EMP>>(empleados);
+        }
+
+        public async Task MetodoNormal()//si el metodo es un void solamente usaremos TASK
+        {
+            //en el codigo interno no hago ninguna peticion asincrona, pero nos permite poder
+            //llamar al metodo, vamos que hay q poner async para llamar al metodo. 
+
+        }
+
+
+
+        //public EMP Get(int id)
+        //{
+        //    return modelo.BuscarEmpleados(id);
+        //}
+
+        public HttpResponseMessage Get(int id) //get del word
+        {
+            EMP empleado = modelo.BuscarEmpleados(id);
+
+            var formato = new JsonMediaTypeFormatter();
+
+            formato.SerializerSettings.DateFormatHandling = Newtonsoft.Json.DateFormatHandling.MicrosoftDateFormat;
+            formato.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+            formato.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            formato.SerializerSettings.Culture = new CultureInfo("es-ES");
+
+            return Request.CreateResponse(HttpStatusCode.OK, empleado, formato);
+        }
+
+    }
+}
